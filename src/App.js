5dd59ca235d930a7554892d405d06fe2fcf36fbe
simeon oyebola <simeonoyekunleoyebola@gmail.com>
2022-12-15 09:33:04 +0100

    components and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+// import { BrowserRouter as Router, Rout, Switch } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
+    <Header />
+    <Footer />
+    <main class="main">
+
+    </main>
     </div>
   );
 }
